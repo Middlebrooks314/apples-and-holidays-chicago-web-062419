443735dee905392fc6d3cf13e8d6fb0367c97310
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-16 23:58:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |holiday, season, decor]
+holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts "  #{holiday.to_s.split("_").collect{|word| word.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
+  end
+end
 
 end
 
